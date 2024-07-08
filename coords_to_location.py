03ee4274e1256d5b417b099e5291482acdabe605
Saraansh Wadkar <saraanshwadkar@gmail.com>
2024-07-08 12:37:14 -0400

    switch to reverse_geocoder for streamlit reasons

--- coords_to_location.py
+++ coords_to_location.py
@@ -1,18 +1,6 @@
-from geopy.geocoders import Nominatim
+import reverse_geocoder as rg
 
 def get_location_data(lat, lon):
-    geolocator = Nominatim(user_agent="metatagger")
-    location = geolocator.reverse((lat, lon), exactly_one=True)
-    address = location.raw['address']
-
-    city = address.get('city', '')
-    if not city:
-        city = address.get('hamlet', '')
-    if not city:
-        city = address.get('town', '')
-    if not city:
-        city = address.get('village', '')
-
-    state = address.get('state', '')
-    country = address.get('country', '')
-    return city, state, country
+    coordinates = (lat, lon)
+    result = rg.search(coordinates)
+    return result[0]['name'], result[0]['admin1'], result[0]['cc']
